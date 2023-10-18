27aa76fdf66b6e42c6dad896a85de3a56b5f875b
Bhanu-code <bhanuchowhan652@gmail.com>
2023-10-18 08:39:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+
+const app = express();
+
+//using json with express
+app.use(express.json());
+
+
+const cors = require('cors')
+
+const mongoose = require('mongoose');
+
+//importing environmental files
+const dotenv = require('dotenv')
+dotenv.config();
+
+//establishing DB connections
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("DB connected.")
+}).catch((err)=>{
+    console.log(err)
+})
+
+// enabling cors
+app.use(cors())
+
+
+//importing routes
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+const paymentRoute = require("./routes/payment")
+
+//specifying routes
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/products", productRoute)
+app.use("/api/cart", cartRoute)
+app.use("/api/order", orderRoute)
+app.use("/api/payment", paymentRoute)
+
+
+//app is listeing at PORT
+app.listen(process.env.PORT, ()=>{
+    console.log('server is up and running ' + process.env.PORT);
+})
+
